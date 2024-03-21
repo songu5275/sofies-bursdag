53aee793157775a4cfbe73b72d06b7e7c6d193dd
Sofie Nhu Nguyen <sofienguyen@Sofie-sin-MacBook-Air.local>
2024-03-21 14:02:53 +0100

    6.

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ export default function Home() {
   return (
     <div className="bg-pink-300 min-h-screen">
       <div className="flex flex-col justify-center items-center">
-        <div className="m-20 text-white"></div>
-        <h1 className="text-4xl m-20">VELKOMMEN TIL SOFIES 21-ÅRS DAG!</h1>
-        <div className="flex gap-10">
-          <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
-          <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
+        <div className="m-20 text-white">
+          <h1 className="text-4xl m-20">VELKOMMEN TIL SOFIES 21-ÅRS DAG!</h1>
+          <div className="flex gap-10">
+            <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
+            <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
+          </div>
         </div>
         <div className="m-20 text-white">
           <p className="font-bold text-1.5xl">TEMA: ____</p>
