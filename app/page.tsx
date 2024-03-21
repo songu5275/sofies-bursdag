0ba83796b2f0f735880713924e0eae290b4bab06
Sofie Nhu Nguyen <sofienguyen@Sofie-sin-MacBook-Air.local>
2024-03-21 13:27:05 +0100

    5.utkast

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <div className="bg-pink-300 min-h-screen">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-4xl m-20">Velkommen til Sofies 21 års dag!</h1>
+        <h1 className="text-4xl m-20">VELKOMMEN TIL SOFIES 21ÅRS DAG!</h1>
         <div className="flex gap-10">
           <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
           <Image src={sofie} alt="Vercel Logo" width={72} height={16} />
         </div>
         <div className="m-20">
-          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>Tema: ____ </p>
-          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>Dato: ____</p>
-          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>Tidspunkt: ____</p>
-          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>Sted: ____</p>
+          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>TEMA: ____ </p>
+          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>DATO: ____</p>
+          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>TIDSPUNKT: ____</p>
+          <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.5rem', color: 'white' }}>STED: ____</p>
         </div>
       </div>
     </div>
