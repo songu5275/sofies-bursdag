77cad94bdda4eaa5cf3f66fb9387c287632884cc
Sofie Nhu Nguyen <sofienguyen@Sofie-sin-MacBook-Air.local>
2024-03-21 14:07:29 +0100

    7.

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
           </div>
         </div>
         <div className="m-20 text-white">
-          <p className="font-bold text-1.5xl">TEMA: ____</p>
-          <p className="font-bold text-1.5xl">DATO: ____</p>
-          <p className="font-bold text-1.5xl">TIDSPUNKT: ____</p>
-          <p className="font-bold text-1.5xl">STED: ____</p>
+          <p className="font-bold text-1.5xl text-white">TEMA: ____</p>
+          <p className="font-bold text-1.5xl text-white">DATO: ____</p>
+          <p className="font-bold text-1.5xl text-white">TIDSPUNKT: ____</p>
+          <p className="font-bold text-1.5xl text-white">STED: ____</p>
         </div>
       </div>
     </div>
