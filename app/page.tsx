4980bc5c50976b1c911061d2d46672733f1d8491
Sofie Nhu Nguyen <sofienguyen@Sofie-sin-MacBook-Air.local>
2024-03-21 14:54:30 +0100

    added confetti

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
+"use client";
+import { useState, useEffect } from 'react';
 import Image from "next/image";
 import sofie from "../app/images/sofie.jpg";
+import minisofie from "../app/images/minisofie.jpg"
+import Confetti from 'react-confetti';
 
 export default function Home() {
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    setShowConfetti(true);
+    setTimeout(() => setShowConfetti(false), 10000); 
+  }, []);
+
   return (
     <div className="bg-pink-300 min-h-screen">
-      <div className="flex flex-col justify-center items-center">
-        <div className="m-20 text-white">
+      {showConfetti && <Confetti />}
+      <div className="flex flex-col justify-center items-center text-white">
           <h1 className="text-4xl m-20">VELKOMMEN TIL SOFIES 21-ÅRS DAG!</h1>
-          <div className="flex gap-10">
-            <Image src={sofie} alt="Sofie" width={72} height={200} />
-            <Image src={sofie} alt="Sofie" width={72} height={200} />
-          </div>
+          <div className="flex flex-col sm:flex-row gap-10"> 
+            <Image src={sofie} alt="Sofie" width={300} height={300} />
+            <Image src={minisofie} alt="Sofie" width={300} height={300} />
         </div>
         <div className="m-20 text-white">
-          <p className="font-bold text-1.5xl text-white">TEMA: ____</p>
+          <p className="font-bold text-1.5xl text-white">TEMA: TBA</p>
           <p className="font-bold text-1.5xl text-white">DATO: ____</p>
           <p className="font-bold text-1.5xl text-white">TIDSPUNKT: ____</p>
           <p className="font-bold text-1.5xl text-white">STED: ____</p>
